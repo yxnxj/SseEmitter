e6388e8e09f334c3fdf6db6f7e83d31a18ca0469
YeonJun Cho <micayel1219@gmail.com>
2022-11-21 20:43:48 +0900

    예제 업로드

---
+++ src/main/java/com/example/sseemitter/SseEmitterApplication.java
@@ -0,0 +1,13 @@
+package com.example.sseemitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseEmitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseEmitterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sseemitter/SseEmitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sseemitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseEmitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
